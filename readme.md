54bb0211530a3511281104be50a91db445f55c8b
estebanherlein <esherlein@gmail.com>
2021-01-19 14:58:38 -0300

    + initial commit + added calculator + added test_calculator, wrote initial tests + added readme.md

---
+++ readme.md
@@ -0,0 +1,6 @@
+In order to test the functionality of pytest I am going to write code that operates as a calculator.
+
+I will write code to test its functionality using pytest.
+
+Then I'm going to automate and optimize the test and delivery process.
+
---
+++ test_calculator.py
@@ -0,0 +1,21 @@
+import calculator
+
+def test_add():
+    result = calculator.add(8, 2)
+    expected = 10
+    assert result == expected
+
+def test_substract():
+    result = calculator.subtract(8, 2)
+    expected = 6
+    assert result == expected
+
+def test_multiply():
+    result = calculator.multiply(8, 2)
+    expected = 16
+    assert result == expected
+
+def test_divide():
+    result = calculator.divide(8, 2)
+    expected = 4
+    assert result == expected
