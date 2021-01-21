009b549d1b3f2d37c90a28450b1800f607d80bed
estebanherlein <esherlein@gmail.com>
2021-01-21 19:52:13 -0300

    + added fixture capabilities to pass multiple parameters to each function

--- v2/calculator.py
+++ v2/calculator.py
@@ -1,12 +1,28 @@
 
+
+def inputsanitization(x, y):
+    if 0 <= x <= 999 and 0 <= y <= 999:
+        return True
+    else:
+        print("Input must be a number between 0 and 999")
+
+
 def add(x, y):
-   return x + y
+    if inputsanitization(x, y):
+        return x + y
+
 
 def subtract(x, y):
-   return x - y
+    if inputsanitization(x, y):
+        return x - y
+
 
 def multiply(x, y):
-   return x * y
+    if inputsanitization(x, y):
+        return x * y
+
 
 def divide(x, y):
-   return x / y
+    if inputsanitization(x, y):
+        return x / y
+
--- v2/test_calculator_v2.py
+++ v2/test_calculator_v2.py
@@ -1,22 +1,33 @@
 import calculator
+import pytest
 
 
-def test_add():
-    result = calculator.add(8, 2)
-    expected = 10
+@pytest.mark.parametrize("first_input,second_input,expected",
+                         [(0, 1, 1), (1, 1, 2), (11, 3, 14), (998, 3, 1001),  (999, 0, 999), pytest.param(-7, 2, 42, marks=pytest.mark.xfail)])
+def test_add(first_input, second_input, expected):
+    result = calculator.add(first_input, second_input)
     assert result == expected
 
-def test_substract():
-    result = calculator.subtract(8, 2)
-    expected = 6
+
+@pytest.mark.parametrize("first_input,second_input,expected",
+                         [(1, 0, 1), (1, 1, 0), (11, 3, 8), (998, 9, 989), (999, 999, 0), pytest.param(4, 1000, 42, marks=pytest.mark.xfail)])
+def test_substract(first_input, second_input, expected):
+    result = calculator.subtract(first_input, second_input)
     assert result == expected
 
-def test_multiply():
-    result = calculator.multiply(8, 2)
-    expected = 16
+
+@pytest.mark.parametrize("first_input,second_input,expected",
+                         [(1, 0, 0), (1, 1, 1), (11, 3, 33), (998, 2, 1996), (999, 999, 998001), pytest.param(5, -999, 42, marks=pytest.mark.xfail)])
+def test_multiply(first_input, second_input, expected):
+    result = calculator.multiply(first_input, second_input)
     assert result == expected
 
-def test_divide():
-    result = calculator.divide(8, 2)
-    expected = 4
+
+@pytest.mark.parametrize("first_input,second_input,expected",
+                         [(0, 1, 0), (1, 1, 1), (12, 3, 4), (998, 2, 499), (999, 333, 3), pytest.param(1002, -101, 42, marks=pytest.mark.xfail)])
+def test_divide(first_input, second_input, expected):
+    result = calculator.divide(first_input, second_input)
     assert result == expected
+
+
+
